f8b5ea9d1d553edf4e636924bfb185441336dfd4
Shloka Bhatt <shlokabhatt22@gmail.com>
2022-08-16 14:22:31 +0530

    :construction:

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,82 +4,58 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+    <div>
+  <div className="main grid place-items-center h-screen bg-cover bg-[url('/img/background.jpg')]">
+    <div
+      id="card"
+      className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 flex p-4 shadow-lg rounded-2xl h-1/2 w-2/5 space-x-5"
+    >
+      <div className="flex flex-col items-center justify-center px-2">
+        <img src="img/circle-cropped.png" alt="" className=" h-60 w-60" />
+      </div>
+      <div className="flex flex-col space-x-3 items-center justify-center">
+        <h1 className="text-5xl font-bold text-white">Shloka Bhatt</h1>
+        <h2 className="text-3xl pt-2 text-white">Web Developer</h2>
+        <ul className="flex flex-row space-x-3 pt-4">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/shloka-bhatt-b9a8a1165/"
+              className="text-white hover:text-blue-700"
+            >
+              <i className="fab fa-linkedin fa-2xl" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/Shlokabhatt"
+              className="text-white hover:text-blue-700"
+            >
+              <i className="fa-brands fa-twitter fa-2xl" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/shloka2212"
+              className="text-white hover:text-blue-700"
+            >
+              <i className="fa-brands fa-github fa-2xl" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/shlokabhatt22/"
+              className="text-white hover:text-blue-700"
+            >
+              <i className="fa-brands fa-facebook fa-2xl" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
+</div>
+
+
   )
 }
 
